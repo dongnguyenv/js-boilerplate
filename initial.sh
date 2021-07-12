2656e53f6f004a2c36db31a0aeafcfeb0856858d
Levi Nguyen <nvdong1989@gmail.com>
2021-07-12 20:52:31 +0700

    add angular boilerplate to workspace

--- initial.sh
+++ initial.sh
@@ -3,7 +3,9 @@
 echo "init submodule ..."
 
 # git submodule add https://github.com/dongnguyenv/react-boilerplate.git src/apps/react-boilerplate
+# git submodule add https://github.com/dongnguyenv/angular-boilerplate.git src/apps/angular-boilerplate
 # git submodule add https://github.com/dongnguyenv/node-api-boilerplate.git src/apis/node-api-boilerplate
+
 git submodule update --init --recursive > /dev/null
 
 echo "update newest from remote ..."
